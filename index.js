58c315cf7361cf855c13baea53c7b59b8e3d9a04
WILMAR ROJAS <wilmarandresrojasvillafane@gmail.com>
2022-10-27 03:51:08 -0500

    EJS

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//motor de plantillas 
+app.set('view engine', 'ejs');
+app.set('views', __dirname +  '/views');
+
+app.use(express.static(__dirname + "/public"));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("app", {titulo : "mi titulo dinámico"})
 })
+
+app.get('/servicios', (req, res) => {
+  res.render("servicios", {tituloSevicios : "Esta es mi pagina de servicios"})
+})
+
+app.use((req, res, next) => {
+  res.status(404).render("404", {
+      titulo: 404,
+      descripcion: "Titulo del sitio web"
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Practica de Express ${port}`)
